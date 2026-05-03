b5e65aa05a1a497649a9a0dd8093a9beb98eda08
thoran <code@thoran.com>
2026-05-04 02:25:41 +1000

    pre-0.0.0: + BinWidth#tuneable_root

--- lib/BinWidth.rb
+++ lib/BinWidth.rb
@@ -2,15 +2,17 @@
 # BinWidth
 
 # 20260416
-# 0.0.0
+# 0.1.0
 
 # Description: Calculates an appropriate bin width for histogramming continuous data.
 
 # Changes:
 # -/0:
-# 1. + Initial version (square_root, freedman_diaconis, scott)
+# 1. + tuneable_root as the general form
+# 2. ~ square_root rewritten as range * n^(-1/2) form
 
 class BinWidth
+
   attr_reader :value
 
   def to_f
@@ -19,50 +21,22 @@ class BinWidth
 
   private
 
-  def initialize(values, method: :square_root)
+  def initialize(values, method: :square_root, factor: 2.0)
     raise ArgumentError, 'Values must not be empty' if values.empty?
     @values = values.map(&:to_f).sort
+    @factor = factor
     @value = send(method)
   end
 
+  def tuneable_root
+    range * @values.size ** (-1.0 / @factor)
+  end
+
   def square_root
-    range / Math.sqrt(@values.size)
-  end
-
-  def freedman_diaconis
-    2.0 * iqr * @values.size ** (-1.0 / 3)
-  end
-
-  def scott
-    3.49 * standard_deviation * @values.size ** (-1.0 / 3)
+    range * @values.size ** (-1.0 / 2)
   end
 
   def range
     @values.last - @values.first
-  end
-
-  def iqr
-    q75 - q25
-  end
-
-  def q25
-    percentile(25)
-  end
-
-  def q75
-    percentile(75)
-  end
-
-  def percentile(p)
-    k = (p / 100.0) * (@values.size - 1)
-    lower = @values[k.floor]
-    upper = @values[k.ceil]
-    lower + (upper - lower) * (k - k.floor)
-  end
-
-  def standard_deviation
-    mean = @values.sum / @values.size
-    variance = @values.map{|v| (v - mean) ** 2}.sum / (@values.size - 1)
-    Math.sqrt(variance)
   end
 end
